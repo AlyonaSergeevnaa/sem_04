6d6b99d52ed6628aeffb3f70fe63ba4ce6e55580
Alyona <molokowa.alyona2010@yandex.ru>
2023-02-09 18:13:00 +0300

    add

---
+++ 2_zadacha/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+System.Console.WriteLine("vvedite chislo: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int result = 0;     
+
+while (i>0)
+{
+    int B=i%10;
+    i=i/10;
+    result +=B;
+
+}
+System.Console.WriteLine("summa = " + result);
+
+
+
+ 
+ 
---
+++ 3_zadacha/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] GetArrayRand (int size, int leftrange, int rightrange)
+{
+    int [] arr_rand = new int [size];
+    Random rand = new Random();
+     
+    for (int i =0; i<size; i++)
+       arr_rand[i] = rand.Next ( leftrange, rightrange+1 );
+    return arr_rand;
+}
+
+
+void PrintTask29(int[] a)
+{
+   System.Console.WriteLine("["+string.Join(", ",arr_rand) +"]");
+}
+
+int[] array = GetArrayRand(12,0,100);
+PrintTask29(array);
